2fa7404e1b7f1f3c708f3c6f10666332d3e2dd35
neurosis69 <83925572+neurosis69@users.noreply.github.com>
2022-01-02 22:53:04 +0100

    Delete run_synctest.sh

--- run_synctest.sh
+++
@@ -1,5 +0,0 @@
-export CHIA_PASSPHRASE_SUPPORT=false
-export ANSIBLE_CALLBACKS_ENABLED=profile_roles
-export ANSIBLE_LOG_PATH=/home/chia/chia-sync-test/log/ansible_playbook.log
-export ANSIBLE_STDOUT_CALLBACK=debug
-ANSIBLE_REMOTE_TEMP=/chia_temp1/.ansible/tmp ansible-playbook synctest.yml 
